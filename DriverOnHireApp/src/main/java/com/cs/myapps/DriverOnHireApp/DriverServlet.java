97804c700973a5323c32a54da908fafe6ef2420d
ChitreshSirohi <chitresh.sirohi@gmail.com>
2016-11-25 09:59:45 +0530

    reverted

--- DriverOnHireApp/src/main/java/com/cs/myapps/DriverOnHireApp/DriverServlet.java
+++ DriverOnHireApp/src/main/java/com/cs/myapps/DriverOnHireApp/DriverServlet.java
@@ -30,7 +30,7 @@ public class DriverServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-   /* @Resource(name = "couchdb/cloudant-DriverSample")
+    @Resource(name = "couchdb/cloudant-DriverSample")
     protected CouchDbInstance _db;
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	String dbname = "my_database";
@@ -54,27 +54,27 @@ public class DriverServlet extends HttpServlet {
     	} catch (Exception e) {
     		response.getWriter().println(e.getMessage());
     	}
-    }*/
+    }
     
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+   /* protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	String dbname = "my_database";
     	CouchDbInstance _db = null;
     	try {
-    	_db = (CouchDbInstance) new InitialContext().lookup("java:comp/env/couchdb/cloudant-sample");
-    	//creates a database with the specified name
-    	CouchDbConnector dbc = _db.createConnector(dbname, true);
-    	response.getWriter().println("Created database: " + dbname);
-    	//create a simple doc to place into your new database
-    	Map<String, Object> doc = new HashMap<String, Object>();
-    	doc.put("_id", UUID.randomUUID().toString());
-    	doc.put("season", "summerChanged");
-    	doc.put("climate", "aridChanged");
-    	dbc.create(doc);
-    	response.getWriter().println("Added a simple doc!");
+    		_db = (CouchDbInstance) new InitialContext().lookup("java:comp/env/couchdb/cloudant-sample");
+    		//creates a database with the specified name
+    		CouchDbConnector dbc = _db.createConnector(dbname, true);
+    		response.getWriter().println("Created database: " + dbname);
+    		//create a simple doc to place into your new database
+    		Map<String, Object> doc = new HashMap<String, Object>();
+    		doc.put("_id", UUID.randomUUID().toString());
+    		doc.put("season", "summerChanged");
+    		doc.put("climate", "aridChanged");
+    		dbc.create(doc);
+    		response.getWriter().println("Added a simple doc!");
     	} catch (Exception e) {
-    	response.getWriter().println(e.getMessage());
+    		response.getWriter().println(e.getMessage());
     	}
-    	}
+    }*/
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
